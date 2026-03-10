dfdb98089e6a0d0039ed77c7253955e9b88b9f4d
Shabeer1024 <shabeer.shukoor@outlook.com>
2026-03-10 20:51:11 +0530

    Ansible

---
+++ main.tf
@@ -0,0 +1,109 @@
+
+  resource "azurerm_resource_group" "NetworkRG" {
+    name     = "NetworkRG"  
+    location = local.resource_location
+  }
+
+  #------------------------------- Virtual Network ----------------------------------------#
+
+
+  resource "azurerm_virtual_network" "Vnet01" {
+    name                = local.virtual_network.name
+    location            = local.resource_location
+    resource_group_name = azurerm_resource_group.NetworkRG.name
+    address_space       = local.virtual_network.address_prefixes
+  }
+
+  resource "azurerm_subnet" "websubnet01" {
+    name                 = local.subnets[0].name
+    resource_group_name  = azurerm_resource_group.NetworkRG.name
+    virtual_network_name = azurerm_virtual_network.Vnet01.name
+    address_prefixes     = local.subnets[0].address_prefixes
+    
+  }
+
+  resource "azurerm_subnet" "appsubnet02" {
+    name                 = local.subnets[1].name
+    resource_group_name  = azurerm_resource_group.NetworkRG.name
+    virtual_network_name = azurerm_virtual_network.Vnet01.name
+    address_prefixes     = local.subnets[1].address_prefixes
+  }
+  #--------------- Network Interface Lion-DC ------------------------
+
+  resource "azurerm_network_interface" "webinterface01" {
+    count = var.vm_count
+    name = "webinterface01-${count.index+1}"
+    location = local.resource_location
+    resource_group_name = azurerm_resource_group.NetworkRG.name
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.websubnet01.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.webip01[count.index].id
+    }
+  }
+
+#---------------------public IP address Lion-Dc------------------
+
+resource "azurerm_public_ip" "webip01" {
+  count               = var.vm_count
+  name                = "webip01-${count.index+1}"
+  resource_group_name = azurerm_resource_group.NetworkRG.name
+  location            = local.resource_location
+  allocation_method   = "Static"
+}
+
+  #-------------------- NSG app-nsg Rules----------------------------
+
+  resource "azurerm_network_security_group" "app-nsg" {
+    name                = "app-nsg"
+    location            = local.resource_location
+    resource_group_name = azurerm_resource_group.NetworkRG.name
+
+    security_rule {
+      name                       = "Allow_SSH"
+      priority                   = 300
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+
+  #------------------NGS Association--------------------------------
+
+  resource "azurerm_subnet_network_security_group_association" "websubnet01_appnsg" {
+    subnet_id                 = azurerm_subnet.websubnet01.id
+    network_security_group_id = azurerm_network_security_group.app-nsg.id
+  }
+
+  #-------------------------- Virtual Mechine -------------------------
+
+  resource "azurerm_linux_virtual_machine" "Lion-DC" {
+    count = var.vm_count
+    name                = "${var.vm_name}-${count.index+1}"
+    resource_group_name = azurerm_resource_group.NetworkRG.name
+    location            = local.resource_location
+    size                = var.vm_size
+    admin_username      = var.admin_username
+    admin_password      = var.admin_password
+    disable_password_authentication = false
+    network_interface_ids = [
+      azurerm_network_interface.webinterface01[count.index].id,
+    ]
+
+    os_disk {
+      caching              = "ReadWrite"
+      storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer     = "ubuntu-24_04-lts"
+      sku       = "server"
+      version   = "latest"
+    }
+  }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+vm_name = "Lion-DC"
+admin_username = "adam"
+admin_password = "D0n0t$hareit"
+vm_size = "Standard_B2s"
