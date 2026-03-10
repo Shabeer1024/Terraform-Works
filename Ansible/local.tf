351cb0a6d6022a65e05123765b27b7086eac8ced
Shabeer1024 <shabeer.shukoor@outlook.com>
2026-03-10 21:03:56 +0530

    Adding ansible

---
+++ Ansible/local.tf
@@ -0,0 +1,18 @@
+locals {
+  resource_location="Central Us"
+  virtual_network={
+    name="Vnet01"
+    address_prefixes=["10.192.0.0/16"]
+  }
+  subnet_address_prefix=["10.192.0.0/24","10.192.1.0/24"]
+  subnets=[
+    {
+        name="websubnet01"
+        address_prefixes=["10.192.0.0/24"]
+    },  
+    {
+        name="appsubnet02"
+        address_prefixes=["10.192.1.0/24"]
+    }
+  ]
+}
