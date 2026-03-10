f988ee93a204ba74e586a61f6e1544ebdbeac807
Shabeer1024 <shabeer.shukoor@outlook.com>
2026-03-10 21:19:34 +0530

    Project-00

---
+++ Project-00/terraform.tfvars
@@ -0,0 +1,3 @@
+vm_name = "Lion-DC"
+admin_username = "adam"
+admin_password = "D0n0t$hareit"
---
+++ Project-00/variables.tf
@@ -0,0 +1,21 @@
+variable "vm_name" {
+type =string
+description = "this is the name for the virtual mechine"  
+}
+
+variable "admin_username" {
+type =string
+description = "This is the admin user name"  
+}
+
+variable "admin_password" {
+type =string
+description = "This is the admin password for the VM"
+sensitive = true  
+}
+
+variable "vm_size" {
+type = string
+description = "This is the size of the Virtual Mechine"
+default = "Standard_B2s"  
+}
