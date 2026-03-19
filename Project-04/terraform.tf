71460fe41eda0f44e6cec6b09813d723070c37a9
Shabeer1024 <shabeer.shukoor@outlook.com>
2026-03-20 02:06:07 +0530

    Update terraform.tf

--- Project-04/terraform.tf
+++ Project-04/terraform.tf
@@ -12,5 +12,5 @@ features {}
 client_id = "be4586aa-4240-4349-b41e-36f08fc1818a"
 client_secret = "L8W8Q~AaRfxDiBrpkEd4LCdzcyZZbY6--SVyRb_F"
 tenant_id = "acea1754-ea16-40e4-af3b-34a7d47ba17d"
-subscription_id = "14ab915d-41b0-4182-8f86-cd71430bb4b1"
+subscription_id = "14ab915d-41b0-4182-8f86-3245456547"
 }
